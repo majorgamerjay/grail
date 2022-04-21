6d90bdcad303e4ccbe75df6c56fd2d59ccc6c614
MajorGamerJay <majorgamerjay@protonmail.com>
2022-04-21 11:27:35 +0600

    add argument parsing

--- src/grail.py
+++ src/grail.py
@@ -1,1 +1,13 @@
 # Made by MajorGamerJay <majorgamerjay@protonmail.com>
+
+# Importing important modules
+import sys
+import argparse
+
+# Declaring argument parser
+parser = argparse.ArgumentParser(prog=f"{sys.argv[0]}", description="Static \
+        website generator using lowdown and python based content management \
+        system")
+parser.add_argument("src", help="Source files directory")
+parser.add_argument("dest", help="Destination documents directory")
+args = parser.parse_args()
