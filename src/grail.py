ee493032d85b9693953e75d8cd194db2f3da5eb9
MajorGamerJay <majorgamerjay@protonmail.com>
2022-04-21 12:13:48 +0600

    defining class for lowdown objects

--- src/grail.py
+++ src/grail.py
@@ -5,6 +5,19 @@ import sys
 import argparse
 import subprocess
 
+
+# Defining MDFile object class
+class MDFile:
+    def __init__(
+            self,
+            path,
+            header,
+            footer
+            ):
+        self.path = path
+        self.header = header
+        self.footer = footer
+
 # Declaring argument parser
 parser = argparse.ArgumentParser(prog=f"{sys.argv[0]}", description="Static \
         website generator using lowdown and python based content management \
